be17fd2a4b2098d6126c3462105aa0f92f46d17a
mc17uulm <marco.combosch@uni-ulm.de>
2020-09-22 21:50:31 +0200

    Initial Commit

---
+++ GeigerCounter/GeigerCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeigerCounterApp.swift
+//  GeigerCounter
+//
+//  Created by Marco Combosch on 22.09.20.
+//
+
+import SwiftUI
+
+@main
+struct GeigerCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
